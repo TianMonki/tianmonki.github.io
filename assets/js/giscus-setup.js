eb30abb0fedc8b4774a60b68dd649d47bc7892af
TianMonki <83906746+TianMonki@users.noreply.github.com>
2026-05-13 12:05:18 +0000

    Deploying to gh-pages from @ TianMonki/tianmonki.github.io@0916087018869dc001580020e963bd139f3a5b45 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "TianMonki/MonkiTian.github.io",
+    "data-repo": "TianMonki/tianmonki.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
